c4df5ed3773e0c633f1013652ab2d3004fa323ff
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-02-24 21:23:55 -0500

    challenge 3 for learnyouanode

--- io.js
+++ io.js
@@ -1,2 +1,11 @@
-var fs = require('fs');
+var commandLineArgs = process.argv;
+var filepath = commandLineArgs[2];
 
+var fs = require('fs');
+var fileContents = fs.readFileSync(filepath);
+
+var fileContentsAsString = fileContents.toString();
+var lines = fileContentsAsString.split('\n');
+var numberOfLines = lines.length - 1;
+
+console.log(numberOfLines);
