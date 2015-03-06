56138a0a8ece2da13a40a4cee48bd6030736dc6d
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2015-03-06 18:51:18 -0500

    refactored time solution

--- time.js
+++ time.js
@@ -2,17 +2,17 @@ var port = process.argv[2];
 var net = require('net');
 
 var server = net.createServer(function(socket){
+	var addTrailingZero = function(number){
+		return number < 10 ? "0" + number : number;
+	};
+
 	var date = new Date();
 	var month = date.getMonth() + 1;
 	var day = date.getDate();
 	var hours = date.getHours();
 	var minutes = date.getMinutes();
 
-	var fullMonth = month < 10 ? "0" + month : month; 
-	var fullDate = day < 10 ? "0" + day : day; 
-	var fullHours = hours < 10 ? "0" + hours : hours;
-	var fullMinutes = minutes < 10 ? "0" + minutes : minutes;	
-	var time = date.getFullYear() + "-" + fullMonth + "-" + fullDate + " " + fullHours + ":" + fullMinutes;
+	var time = date.getFullYear() + "-" + addTrailingZero(month) + "-" + addTrailingZero(day) + " " + addTrailingZero(hours) + ":" + addTrailingZero(minutes);
 	socket.end(time);
 });
 server.listen(Number(port));
